e4666f021e2fd3498b17c17c8aaf7b59eedebde2
Nicholas Deng <nicholas.deng@worldrapidfinance.com>
2021-04-28 16:34:30 -0400

    Another user route

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,12 @@ router.get('/', function(req, res, next) {
   res.send(users);
 });
 
+router.get('/:id', function(req, res, next) {
+  var userId = req.params.id;
+  console.log("USER ID:", userId)
+
+  var user = {"id": userId, "name": "Example User", "email": "example@example.com"} // just some dummy data
+  res.send(user);
+});
+
 module.exports = router;
